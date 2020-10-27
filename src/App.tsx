e17ec91a7e3d7deb66980ce0cec03cc6f206fb5b
Jess <shaderone.jd@gmail.com>
2020-10-27 11:53:38 -0700

    Initial framework for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>
+          Hi!  I'm Jess Desrochers.
+        </h1>
+        <h3>
+          I'm a full-stack software developer from Vancouver, Canada
+        </h3>
+      </header>
+
+      <a
+        className="App-link"
+        href="https://www.linkedin.com/in/jessdesr/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        className="App-link"
+        href="https://www.github.com/jessdesr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      <a
+        className="App-link"
+        href="https://codepen.io/jessdesr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Codepen
+      </a>
+      <h2>Projects</h2>
+      <h2>Contact</h2>
     </div>
   );
 }
